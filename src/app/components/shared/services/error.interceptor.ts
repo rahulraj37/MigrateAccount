651e0fe77c4cc92dd8b0b8cc50ea18674dd28edf
Rahulraj <Rahulraj.rajaganesh@taylorandfrancis.com>
2024-03-12 15:14:33 +0530

    updates

--- src/app/components/shared/services/error.interceptor.ts
+++ src/app/components/shared/services/error.interceptor.ts
@@ -1,5 +1,14 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
 
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+  return next(req).pipe
+  (catchError((error) =>{
+    if ([404].includes (error.status)){
+      // console.log('API not found');
+    }
+    const e=error.error.message || error.statusText;
+    console.log(e);
+    return throwError ( () => error);
+  }));
 };
