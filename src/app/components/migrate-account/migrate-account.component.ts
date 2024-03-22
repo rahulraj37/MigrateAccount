d6e50c229298e107ac66aec355a28194fec150ca
Rahulraj <Rahulraj.rajaganesh@taylorandfrancis.com>
2024-03-22 15:59:18 +0530

    update

--- src/app/components/migrate-account/migrate-account.component.ts
+++ src/app/components/migrate-account/migrate-account.component.ts
@@ -1,59 +1,110 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { Subscription } from 'rxjs';
 import {MatIconModule} from '@angular/material/icon';
 import { HttpService } from '../shared/services/http.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+// export interface Job {
+//   jobID: number;
+//   title: string;
+//   company: string;
+//   location: string;
+//   description: string;
+//   requirements: string;
+//   contactEmail: string;
+// }
 
-
-export interface Job {
-  jobID: number;
-  title: string;
-  company: string;
-  location: string;
-  description: string;
-  requirements: string;
-  contactEmail: string;
+interface Submission {
+  submissionNumber: string;
+  accountType: string;
 }
 
 @Component({
   selector: 'app-migrate-account',
   standalone: true,
-  imports: [MatTableModule,MatIconModule],
+  imports: [MatTableModule,MatIconModule,FormsModule,CommonModule],
   templateUrl: './migrate-account.component.html',
   styleUrl: './migrate-account.component.scss'
 })
-export class MigrateAccountComponent implements OnInit {
-  
-  displayedColumns: string[] = ['jobID', 'title', 'company', 'location', 'description', 'requirements', 'contactEmail'];
-  dataSource = new MatTableDataSource<Job>();
-  subscription: Subscription | undefined;
+export class MigrateAccountComponent {
+  emailAddress: string = '';
+  emailAddress2: string = '';
+  accountType: string = '';
+  submissions: Submission[] = [];
+  emailFetched: boolean = false;
+  emailValidationMessage: string = '';
+  isNewEmailValidated: boolean = false;
+  displayedColumns: string[] = ['submissionNumber', 'accountType'];
+  dataSource: MatTableDataSource<Submission>;
 
-  constructor(private httpService: HttpService) { }
-
-  ngOnInit(): void {
-    // this.getJobs();
+  constructor(private http: HttpClient) {
+    this.dataSource = new MatTableDataSource<Submission>(this.submissions);
   }
 
-  getJobs() {
-    this.subscription = this.httpService.get('/jobs')
-      .subscribe({
-        next: (response: Job[]) => {
-          this.dataSource.data = response;
-        },
-        error: (error: any) => {
-          console.error('Error fetching jobs:', error);
-        }
-      });
-  }
-  
-  search(){
-    this.getJobs();
+  fetchDetails() {
+    // Assuming API endpoint to fetch submissions
+    const apiUrl = 'https://example.com/api/submissions';
+
+    this.http.get<Submission[]>(apiUrl).subscribe(
+      (data) => {
+        this.submissions = data;
+        this.dataSource.data = this.submissions;
+      },
+      (error) => {
+        console.error('Error fetching submissions:', error);
+      }
+    );
   }
 
-  ngOnDestroy(): void {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
-    }
+  submitOldEmail(emailValue: string) {
+    console.log('Submitted old email:', emailValue);
+    // Here you can perform any additional actions with the submitted old email address
   }
 
+  submitNewEmail(emailValue: string) {
+    console.log('Submitted new email:', emailValue);
+    // Here you can perform any additional actions with the submitted new email address
+    // For now, let's assume validation is successful
+    this.isNewEmailValidated = true;
+
+    // Fetch submissions for the new email address
+    this.fetchDetails();
+  }
 }
+// {
+  
+//   displayedColumns: string[] = ['jobID', 'title', 'company', 'location', 'description', 'requirements', 'contactEmail'];
+//   dataSource = new MatTableDataSource<Job>();
+//   subscription: Subscription | undefined;
+
+//   constructor(private httpService: HttpService) { }
+
+//   ngOnInit(): void {
+//     // this.getJobs();
+//   }
+
+//   getJobs() {
+//     this.subscription = this.httpService.get('/jobs')
+//       .subscribe({
+//         next: (response: Job[]) => {
+//           this.dataSource.data = response;
+//         },
+//         error: (error: any) => {
+//           console.error('Error fetching jobs:', error);
+//         }
+//       });
+//   }
+  
+//   search(){
+//     this.getJobs();
+//   }
+
+//   ngOnDestroy(): void {
+//     if (this.subscription) {
+//       this.subscription.unsubscribe();
+//     }
+//   }
+
+// }
