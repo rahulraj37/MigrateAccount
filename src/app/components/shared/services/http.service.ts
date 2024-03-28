cd86d650331f604bc8cf7423a70d8d26e54cc892
Rahulraj <Rahulraj.rajaganesh@taylorandfrancis.com>
2024-03-28 16:24:41 +0530

    updates

--- src/app/components/shared/services/http.service.ts
+++ src/app/components/shared/services/http.service.ts
@@ -1,21 +1,22 @@
 import { HttpClient} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-
+import { environment } from '../../../../environments/environment'
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
-  private baseUrl = 'https://monacodelisa-node-express.cyclic.app/api';
+  apiUrl = environment.apiUrl;
+
 
   constructor(private httpClient: HttpClient) { }
 
   get(url: string, params?: any): Observable<any> {
-    return this.httpClient.get(this.baseUrl + url, { params });
+    return this.httpClient.get(this.apiUrl + url, { params });
       }
 
   post(url: string, payload: any): Observable<any> {
-    return this.httpClient.post(this.baseUrl + url, payload);
+    return this.httpClient.post(this.apiUrl + url, payload);
        }
 
 }
